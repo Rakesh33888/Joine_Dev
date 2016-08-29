afd75cf0137f5f087b27fc67996b2243aae3ab0b
ajay <ajay.jaglan@brahmasys.com>
2016-08-29 13:09:40 +0530

    Remove the custom_dialog ,complete the leave chat
    Remove the custom_dialog ,complete the leave chat

--- app/src/main/java/com/brahmasys/bts/joinme/Single_group_Message.java
+++ app/src/main/java/com/brahmasys/bts/joinme/Single_group_Message.java
@@ -1,11 +1,11 @@
 package com.brahmasys.bts.joinme;
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -15,10 +15,11 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.brahmasys.bts.joinme.R;
 import com.github.siyamed.shapeimageview.CircularImageView;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -28,9 +29,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.InputStream;
-import java.io.StringReader;
-
 
 public class Single_group_Message extends Fragment    {
 
@@ -38,15 +36,21 @@ public class Single_group_Message extends Fragment    {
     private static final String ARG_PARAM2 = "param2";
     public static final String USERID = "userid";
     public static final String ACTIVITYID = "activity_id";
+    private static final String URL_RemoveMemberToGroup = "http://52.37.136.238/JoinMe/Activity.svc/RemoveMemberToGroup/";
     private static final String URL_GetActivityDetailsForChat = "http://52.37.136.238/JoinMe/Activity.svc/GetActivityDetailsForChat/";
+
+
+
+
     private static final String IMAGE_BASE_URL = "http://52.37.136.238/JoinMe/";
 
 
     CircularImageView createrimage;
+    Button yes,no;
 
     SharedPreferences user_id,activity_id;
     SharedPreferences.Editor edit_userid,edit_activity_id;
-    TextView tvActivityName,tvActivityTime,tvActivityAddress,tvHostedByName;
+    TextView tvActivityName,tvActivityTime,tvActivityAddress,tvHostedByName,tvleave_chat;
     private String mParam1;
     private String mParam2;
 
@@ -55,8 +59,8 @@ public class Single_group_Message extends Fragment    {
     public Single_group_Message() {
         // Required empty public constructor
     }
-   TextView leave_chat,name;
-    Custom_dialoge dialoge;
+   //TextView name;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -73,25 +77,56 @@ public class Single_group_Message extends Fragment    {
 
         View v = inflater.inflate(R.layout.fragment_single_group__message, container, false);
 
-        leave_chat = (TextView) v.findViewById(R.id.leave_chat);
+
         createrimage = (CircularImageView) v.findViewById(R.id.createrimage);
         tvActivityName= (TextView) v.findViewById(R.id.textView25);
         tvActivityTime= (TextView) v.findViewById(R.id.textView26);
+        tvleave_chat= (TextView) v.findViewById(R.id.leave_chat);
         tvActivityAddress = (TextView) v.findViewById(R.id.textView27);
         tvHostedByName = (TextView) v.findViewById(R.id.name);
+
+
+
 
         user_id =getActivity().getSharedPreferences(USERID, getActivity().MODE_PRIVATE);
         edit_userid = user_id.edit();
         activity_id = getActivity().getSharedPreferences(ACTIVITYID, getActivity().MODE_PRIVATE);
         edit_activity_id = activity_id.edit();
-
-        leave_chat.setPaintFlags(leave_chat.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
-        leave_chat.setOnClickListener(new View.OnClickListener() {
+        tvleave_chat.setPaintFlags(tvleave_chat.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+        tvleave_chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Custom_dialoge dialogFragment = new Custom_dialoge ();
-                dialogFragment.show(getActivity().getFragmentManager(),"simple dialog");
+                final Dialog dialog = new Dialog(getActivity());
+                dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+                dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                dialog.setContentView(R.layout.custom_dialog);
+                dialog.getWindow().setBackgroundDrawable(
+                        new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+                yes = (Button) dialog.findViewById(R.id.yes);
+                no = (Button) dialog.findViewById(R.id.no);
+                yes.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+
+                        FnLeaveChat();
+
+
+                       dialog.dismiss();
+                    }
+                });
+                no.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+
+
+
 
 
             }
@@ -149,6 +184,7 @@ public class Single_group_Message extends Fragment    {
 
                                     for(int i=0; i<arrGroup.length(); i++) {
 
+
                                         JSONObject row = arrGroup.getJSONObject(i);
                                         if(row.getBoolean("isowner")){
                                             tvHostedByName.setText(row.getString("user_name"));
@@ -173,8 +209,35 @@ public class Single_group_Message extends Fragment    {
 
         return v;
     }
+    private void FnLeaveChat() {
+        String userid = user_id.getString("userid", "");
+        String act_id = activity_id.getString("activity_id", "");
 
-    // TODO: Rename method, update argument and hook method into UI event
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(URL_RemoveMemberToGroup + userid + "/" + act_id,
+                new AsyncHttpResponseHandler() {
+                    // When the response returned by REST has Http response code '200'
+
+                    public void onSuccess(String response) {
+                        Messagescreen fragment2 = new Messagescreen();
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.flContent, fragment2);
+                        fragmentTransaction.commit();
+
+                    }
+
+
+
+                });
+
+
+    }
+
+
+
+
+        // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
